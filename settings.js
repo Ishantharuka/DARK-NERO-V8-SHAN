94a8087aef3503c32bf5b5cc6493835d112e5379
Ishantharuka <127075604+Ishantharuka@users.noreply.github.com>
2023-03-31 10:23:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "d3d3dXBjdUg=" //session Id
+global.session = "" //session Id
 global.ownernomer = "94764990352" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['+94 71 420 6741'] //ur premium number
